c5ee5699f54e83c8475e222f9ba023aa43440072
Виталий Шебаниц <mactepwar@outlook.com>
2018-12-25 00:34:49 +0200

    first commit

---
+++ Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountSystem.Data
+{
+
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseMySql("server=localhost;user id=root;password=admin;database=mysite");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+
+    }
+}
---
+++ EmailService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace AccountSystem.Models
+{
+    public class EmailService
+    {
+        public static async Task SendEmailAsync(string email, string subject, string message)
+        {
+            MailMessage mail = new MailMessage("shebanic1995@gmail.com", email);
+            mail.Body = message;
+            mail.Subject = subject;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl = true;
+            smtp.Credentials = new NetworkCredential("shebanic1995@gmail.com", "Roma2005accessdenied");
+            try
+            {
+                await smtp.SendMailAsync(mail);
+                //Page.RegisterStartupScript("UserMsg", "<script>alert('Successfully Send...');if(alert){ window.location='SendMail.aspx';}</script>");
+            }
+            catch (Exception ex)
+            {
+                Exception ex2 = ex;
+                string errorMessage = string.Empty;
+                while (ex2 != null)
+                {
+                    errorMessage += ex2.ToString();
+                    ex2 = ex2.InnerException;
+                }
+                //Page.RegisterStartupScript("UserMsg", "<script>alert('Sending Failed...');if(alert){ window.location='SendMail.aspx';}</script>");
+            }
+        }
+    }
+}
